4f08e46d6abec6c31e3e82c7728fba23b779d5e1
michael.cardenas <michael.cardenas@realtechltda.com>
2025-05-22 08:07:42 -0500

    ingles primero

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,30 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PageController::class, 'showEnglish'])->name('home');
 
+// Dashboard
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// Perfil
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+// Cambio de idioma
 Route::get('locale/{locale}', function ($locale) {
     session()->put('locale', $locale);
     return redirect()->back();
 })->name('locale');
-Route::get('/jufman', [App\Http\Controllers\PageController::class, 'showEnglish'])->name('jufman');
+
+// Ruta explícita en inglés si deseas mantenerla también
+Route::get('/jufman', [PageController::class, 'showEnglish'])->name('jufman');
+
 require __DIR__.'/auth.php';
