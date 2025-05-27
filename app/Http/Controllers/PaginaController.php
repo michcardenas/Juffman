c2a47ed4fc49683ce729a0c479f6695955790642
michael.cardenas <michael.cardenas@realtechltda.com>
2025-05-26 22:50:26 -0500

    Brandi

--- app/Http/Controllers/PaginaController.php
+++ app/Http/Controllers/PaginaController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
-use App\Http\Controllers\GlobalSetting;
+use App\Models\GlobalSetting;
 
 
 
